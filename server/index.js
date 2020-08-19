6cc839444843175447012e173b51aa265e1de23d
Fabiano Barbosa <fabiano.siqueirab@gmail.com>
2020-08-19 18:42:56 -0300

    configured endpoints

--- server/index.js
+++ server/index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const yup = require('yup');
+const monk = require('monk');
+const { nanoid } = require('nanoid');
+const { restart } = require('nodemon');
+
+require('dotenv').config();
+
+const db = monk(process.env.MONGO_URI);
+const urls = db.get('urls');
+urls.createIndex({ slug: 1 }, { unique: true });
 
 const app = express();
+
 
 app.use(helmet());
 app.use(morgan('tiny'));
@@ -16,13 +27,68 @@ app.use(express.static('./public'));
 //     // TODO: get a short url by id to URL
 // });
 
-// app.get('/:id', (req, res) => {
-//     // TODO: redirect to URL
-// });
+app.get('/:id', async (req, res) => {
+    const { id: slug } = req.params;
+    try {
+        const url = await urls.findOne({ slug });
+        if (url) {
+            res.redirect(url.url);
+        }
+        res.redirect('/?error=${slug} not found');
+    } catch (error) {
+        res.redirect('/?error=Link not found');
+    }
+});
 
-// app.post('/url', (req, res) => {
-//     // TODO: create a short url
-// });
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().trim().url().required()
+});
+
+app.post('/url', async (req, res, next) => {
+    let { slug, url } = req.body;
+    try {
+        await schema.validate({
+            slug,
+            url,
+        });
+        if (!slug) {
+            slug = nanoid(5);
+        } else {
+            const existing = await urls.findOne({ slug });
+            if (existing) {
+                throw new Error('Slug in use. 🍔');
+            }
+        }
+        slug = slug.toLowerCase();
+        // const secret = nanoid(10).toLowerCase();
+        const newUrl = {
+            url,
+            slug,
+            // secret,
+        };
+        // res.json({
+        //     slug,
+        //     url,
+        // })
+        const created = await urls.insert(newUrl);
+        res.json(created);
+    } catch (error) {
+        next(error);
+    };
+});
+
+app.use((error, req, res, next) => {
+    if (error.status) {
+        res.status(error.status);
+    } else {
+        res.status(500);
+    }
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV == 'production' ? '🥞' : error.stack,
+    });
+});
 
 
 
