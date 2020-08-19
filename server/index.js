347691fdee10066308ea5dda2c190bb0951f64af
Fabiano Barbosa <fabiano.siqueirab@gmail.com>
2020-08-19 04:22:29 -0300

    now configuration

--- server/index.js
+++ server/index.js
@@ -9,12 +9,22 @@ app.use(helmet());
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
+app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'fsb.sh - Short Urls for your code garden'
-    });
-});
+
+// app.get('/url/:id', (req, res) => {
+//     // TODO: get a short url by id to URL
+// });
+
+// app.get('/:id', (req, res) => {
+//     // TODO: redirect to URL
+// });
+
+// app.post('/url', (req, res) => {
+//     // TODO: create a short url
+// });
+
+
 
 const port = process.env.PORT || 1337;
 
